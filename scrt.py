4fde0482a2bc08f68ce601b4f3ae8209639ba0ab
nicol <nicolas.mahn@gmx.de>
2025-05-21 19:41:56 +0200

    v1.4 fixed ci/cd, added DinD config

--- scrt.py
+++ scrt.py
@@ -1,5 +1,4 @@
 import os
-# "ec2-13-53-139-90.eu-north-1.compute.amazonaws.com"
-BACKEND_HOST = os.getenv("BACKEND_HOST", "ec2-13-53-139-90.eu-north-1.compute.amazonaws.com") #"127.0.0.1")
+BACKEND_HOST = os.getenv("BACKEND_HOST", "127.0.0.1")
 BACKEND_PORT = int(os.getenv("BACKEND_PORT", 5000))
 FRONTEND_KEY = os.getenv("FRONTEND_KEY")
